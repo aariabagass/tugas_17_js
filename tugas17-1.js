9b20131a3934b78f1d40abf79a3f54a0ac66db42
Aria Bagas <aariabagass@gmail.com>
2019-08-22 23:38:00 +0700

    JS Task 17
    string lowercase, uppercase, char code at

---
+++ tugas17-1.js
@@ -0,0 +1,10 @@
+var upperLower = () => {
+  first = 'Saya beLajar bahaSa peMrograman deNgan khUsuk.';
+  second = 'Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.';
+  console.log(first.toLowerCase());
+  console.log(first.toUpperCase());
+  console.log(second.toLowerCase());
+  console.log(second.toUpperCase());
+  return upperLower;
+}
+upperLower();
